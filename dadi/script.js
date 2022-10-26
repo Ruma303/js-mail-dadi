c6d9ecd5e7259006fffe1a662c850d4de082aa62
Ruma <matteoh8@hotmail.it>
2022-10-26 10:03:54 +0200

    aggiornamento codice

--- dadi/script.js
+++ dadi/script.js
@@ -1,34 +1,24 @@
-/*Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Che ci sia un array da qualche parte?
-Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-Consigli del giorno:
-1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-3. si ma noi cosa vogliamo fare?
-4. torniamo a scrivere in italiano
-5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"*/
+const eleUserNumber = document.querySelector('#user-number');
+const elePcNumber = document.querySelector('#pc-number');
+const eleUserOutput = document.querySelector('#user-output');
+const elePcOutput = document.querySelector('#pc-output');
+const eleWinner = document.querySelector('#winner');
 
 
-const numberList = [1, 2, 3, 4, 5, 6];
 
-let numUser = parseInt(prompt('Inserisci un numero intero da 1 a 6'));
-let numPC = Math.floor(Math.random()* (6 - 1 + 1)) + 1;6
+// Generazione numeri
+const UserNumber = Math.floor(Math.random()* 6) + 1;
+const PcNumber = Math.floor(Math.random()* 6) + 1;
 
-console.log('Hai scelto il numero ' + numUser);
-console.log('Il computer ha scelto il numero ' + numPC);
+console.log('Hai scelto il numero ' + UserNumber);
+console.log('Il computer ha scelto il numero ' + PcNumber);
 
-for (let i = 1; i<=6; i++) {
-    if (isNaN(numUser) != !isNaN){
-        console.log('Non hai scelto un numero, con te non ci gioco più :(');
-    } else if (numUser > numPC) {
-        console.log('Hai vinto!');
-    } else if (numUser < numPC) {
-        console.log('Il computer ha vinto!');
-    } else if (numUser === numPC) {
-        console.log('Pareggio!');
-    }
+if (isNaN(UserNumber) == isNaN){
+    console.log('Non hai scelto un numero, con te non ci gioco più :(');
+} else if (UserNumber > PcNumber) {
+    console.log('Hai vinto!');
+} else if (UserNumber < PcNumber) {
+    console.log('Il computer ha vinto!');
+} else if (UserNumber === PcNumber) {
+    console.log('Pareggio!');
 }
